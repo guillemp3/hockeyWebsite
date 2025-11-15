caaa9b855846d8080a06eb5f020ca0cf7a84618c
Guillaume Faucher <guillaumefaucher05@gmail.com>
2025-11-15 15:15:41 -0500

    init tailwind usage

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./*.html", "./*.js"], // scan all HTML and JS files in current folder
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
